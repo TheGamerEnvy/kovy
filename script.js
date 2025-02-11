5eda6e7d4f1d0ffebccaaaf474ca8d2650506d9c
Envy <aagen02@gmail.com>
2025-02-11 14:33:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function showKomik(komikId) {
-  // Sembunyikan semua komik
-  var komikContents = document.querySelectorAll('.komik-content');
-  komikContents.forEach(function(komik) {
-    komik.style.display = 'none';
-  });
+    // Sembunyikan semua komik
+    var komikContents = document.querySelectorAll('.komik-content');
+    komikContents.forEach(function(komik) {
+        komik.style.display = 'none';
+    });
 
-  // Tampilkan komik yang dipilih
-  var selectedKomik = document.getElementById(komikId);
-  selectedKomik.style.display = 'block';
+    // Tampilkan komik yang dipilih
+    var selectedKomik = document.getElementById(komikId);
+    if (selectedKomik) {
+        selectedKomik.style.display = 'block';
+    }
 }
